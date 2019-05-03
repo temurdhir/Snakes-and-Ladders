ed498e3f6b4acf6c81808c275990cadaacec423d
temur <temur@DESKTOP-O3F9GFL>
2019-05-03 15:03:45 +0530

    done

--- src/main/java/main/SnakeLadders.java
+++ src/main/java/main/SnakeLadders.java
@@ -12,12 +12,12 @@ public class SnakeLadders{
 	}
 	public static boolean snake(int i,int j) {
 		//snake is in 4th place thats the co-ordinate [1,2] i.e, i=1 & j=2
-		if(i==1 && j==2) {
+		if(i==1&&j==2) {
 			return true;
 		}
 		
 		//write your code here
-		return false;
+		return  false;
 	}
 	public static boolean ladder(int i,int j) {
 		//ladder is in 3rd place thats the co-ordinate [0,3] i.e, i=0 and j=2
